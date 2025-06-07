4e918c476d679838986b4b2ddf0507f619b3e12c
Nicat667 <nijatvo@code.edu.az>
2025-06-08 01:28:40 +0400

    updates in room detail

--- Travel/obj/Debug/net8.0/Travel.AssemblyInfo.cs
+++ Travel/obj/Debug/net8.0/Travel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68b92257960fd78e83b24459801603aa9dfa6cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aeba21f691688ea3e02db7ef7e5518e1a4e9211")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
